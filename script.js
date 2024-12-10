7f7a6fc9d20023a5463aa7db60a83a5501925244
Abdul-Ahadexpo <adahadd4845e@gmail.com>
2024-12-11 01:21:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ let cart = [];
 
 // Sample product data
 const products = [
+{
+  id: 92.12,
+  name: "180TK Beyblade X Mystery Box",
+  description: "An exciting mystery box filled with Beyblade X surprises worth more than the price you pay!",
+  price: 180,
+  pcs: "∞",
+  image: "https://thebeyverse.com/cdn/shop/products/6C495ED9-94FD-43FD-A2D2-1630AD50E3E8.jpg?v=1680825615",
+}
+
   {
     id: 2.1,
     name: "BX-34 Cobalt Dragoon 2-60C",
