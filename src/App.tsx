8d549ddaf458c3470d6ff972395ab5f67e2b0794
Abdul Ahad <adahadd4845e@gmail.com>
2025-05-07 13:21:17 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import HelpGuide from './components/HelpGuide';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Admin from './pages/Admin';
 import Checkout from './pages/Checkout';
 import Reviews from './pages/Reviews';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
@@ -16,13 +16,14 @@ function App() {
       <Router>
         <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
           <Navbar />
-          <div className="pb-24"> {/* Added padding to account for footer */}
+          <div className="pb-24">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/admin" element={<Admin />} />
               <Route path="/checkout" element={<Checkout />} />
               <Route path="/reviews" element={<Reviews />} />
+              <Route path="/product/:id" element={<ProductDetails />} />
             </Routes>
           </div>
           <HelpGuide />
